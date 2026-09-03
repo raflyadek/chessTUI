9c341b96ffaeef20caa3db42f8c75e1883020173
rafly <raflyade9@gmail.com>
2026-09-03 22:20:04 +0700

    chore

--- app/main.go
+++ app/main.go
@@ -1123,6 +1123,7 @@ func moveState(pieceLocation string, fromRow, toRow, fromCol, toCol int, board [
 
 func afterCheckMate() string {
 	fmt.Print("do you want to play again (p) or exit (e)? ")
+	fmt.Println()
 	reader := bufio.NewScanner(os.Stdin)
 	reader.Scan()
 	choice := reader.Text()
